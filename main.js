8fef23677926bf50b89a6ad89cd1f587f547f9ab
Tyler Wong <tyler.wong@liferay.com>
2016-10-07 12:10:38 -0700

    js-colors added

---
+++ main.js
@@ -0,0 +1,78 @@
+// a,s,d,f
+addEventListener("keydown", function(event) {
+    if (event.keyCode == 65)
+      document.body.style.background = "red";
+});
+addEventListener("keyup", function(event) {
+    if (event.keyCode == 65)
+      document.body.style.background = "";
+});
+
+addEventListener("keydown", function(event) {
+    if (event.keyCode == 83)
+      document.body.style.background = "orange";
+});
+addEventListener("keyup", function(event) {
+    if (event.keyCode == 83)
+      document.body.style.background = "";
+});
+
+addEventListener("keydown", function(event) {
+    if (event.keyCode == 68)
+      document.body.style.background = "yellow";
+});
+addEventListener("keyup", function(event) {
+    if (event.keyCode == 68)
+      document.body.style.background = "";
+});
+
+addEventListener("keydown", function(event) {
+    if (event.keyCode == 70)
+      document.body.style.background = "green";
+});
+addEventListener("keyup", function(event) {
+    if (event.keyCode == 70)
+      document.body.style.background = "";
+});
+
+  65, 83, 68, 70
+
+  74,75,76,186
+// j,k,l,;
+addEventListener("keydown", function(event) {
+    if (event.keyCode == 74)
+      document.body.style.background = "blue";
+});
+addEventListener("keyup", function(event) {
+    if (event.keyCode == 74)
+      document.body.style.background = "";
+});
+
+addEventListener("keydown", function(event) {
+    if (event.keyCode == 75)
+      document.body.style.background = "indigo";
+});
+addEventListener("keyup", function(event) {
+    if (event.keyCode == 75)
+      document.body.style.background = "";
+});
+
+addEventListener("keydown", function(event) {
+    if (event.keyCode == 76)
+      document.body.style.background = "violet";
+});
+addEventListener("keyup", function(event) {
+    if (event.keyCode == 76)
+      document.body.style.background = "";
+});
+
+addEventListener("keydown", function(event) {
+    if (event.keyCode == 186)
+      document.body.style.background = "white";
+});
+addEventListener("keyup", function(event) {
+    if (event.keyCode == 186)
+      document.body.style.background = "";
+});
+
+
